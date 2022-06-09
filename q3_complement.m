1760692c8525210b0ca670568f379a743240237e
mohammedyousuf <mohammedyousuf051@gmail.com>
2022-06-09 13:17:59 +0800

    Set 1

--- q3_complement.m
+++ q3_complement.m
@@ -6,16 +6,16 @@ img = imresize(grayBaboon,1.30);
 imshow(img);
 subplot(1,3,1);
 
-imshow(img);
+// imshow(img);
 
-subplot(1,3,2);
-invGrayLena1 = 255 - img;
-imshow(invGrayLena1);
+// subplot(1,3,2);
+// invGrayLena1 = 255 - img;
+// imshow(invGrayLena1);
 
-subplot(1,3,3);
-invGrayLena2 = imcomplement(img);
-imshow(img);
-imwrite(img,'complementedBaboon.jpg','jpg');
+// subplot(1,3,3);
+// invGrayLena2 = imcomplement(img);
+// imshow(img);
+// imwrite(img,'complementedBaboon.jpg','jpg');
 
 
 
