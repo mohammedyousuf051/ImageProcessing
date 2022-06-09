469fb08ab011e4bd5b6975cb76dd3018346b499a
mohammedyousuf <mohammedyousuf051@gmail.com>
2022-06-09 13:17:09 +0800

    Set 1

--- q1_bitplane.m
+++ q1_bitplane.m
@@ -1,4 +1,4 @@
 grayLena = imread('gray_Lenna','png');
 plane4Lena = bitget(grayLena,7);
-// imshow(logical(plane4Lena));
-// imwrite(logical(plane4Lena),'q1_result.jpg','jpg');
+imshow(logical(plane4Lena));
+imwrite(logical(plane4Lena),'q1_result.jpg','jpg');
