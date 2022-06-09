efff10c84399258fa5b5ea14743df9d2d657c713
mohammedyousuf <mohammedyousuf051@gmail.com>
2022-06-09 13:17:40 +0800

    Set 1

--- q2_binaryconverstion.m
+++ q2_binaryconverstion.m
@@ -1,7 +1,7 @@
 
 I = imread('Lenna','png');
 imshow(I);
-// thresh = 160/255;     %threshold
-// I2 = im2bw(I,thresh);       
-// imshow(I2);
-// imwrite(I2,'result.jpg','jpg');
+thresh = 160/255;     %threshold
+I2 = im2bw(I,thresh);       
+imshow(I2);
+imwrite(I2,'result.jpg','jpg');
